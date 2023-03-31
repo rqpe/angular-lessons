dbb3fce588b5c648b1c2ca9f1c0dcd2b21c20e29
David Carvajal Garrido <davidcarvajalg@gmail.com>
2023-03-31 20:35:37 +0200

    Event binding example

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -8,7 +8,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   message = '';
-  domingo: boolean = true
+  domingo: boolean = true;
+  myData = '';
+
+  statusBtn = false;
 
   constructor() {
     if(this.domingo) {
@@ -16,6 +19,18 @@ export class AppComponent {
     }
     else {
       this.message = 'A trabajar';
+    }
+  }
+
+  showData() {
+    if(this.statusBtn === true) {
+
+      this.myData = 'Que tengáis muy buen finde';
+      this.statusBtn = false;
+    }
+    else {
+      this.myData = 'No hay nada que decir';
+      this.statusBtn = true;
     }
   }
 }
